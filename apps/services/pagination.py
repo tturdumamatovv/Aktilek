c4f0f947a582cc64327b56ae947d5fcf6a7a62f2
Nurmuhammed <muki2022@mail.ru>
2024-10-18 16:46:24 +0600

    a

--- apps/services/pagination.py
+++ apps/services/pagination.py
@@ -3,23 +3,12 @@ from rest_framework.response import Response
 
 
 class CustomPagination(PageNumberPagination):
-    page_size_query_param = 'page_size'  # Позволяет клиенту передавать page_size через запрос
-    max_page_size = 100  # Устанавливаем максимальный размер страницы
+    page_size = 10
 
     def get_paginated_response(self, data):
-        # Проверяем, что self.page и self.page.paginator существуют
-        if not self.page:
-            return Response({
-                'count': 0,
-                'total_pages': 0,
-                'next': None,
-                'previous': None,
-                'results': [],
-            })
-
         return Response({
-            'count': self.page.paginator.count if self.page.paginator else 0,
-            'total_pages': self.page.paginator.num_pages if self.page.paginator else 0,  # Добавляем количество страниц
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,  # Добавляем количество страниц
             'next': self.get_next_link(),
             'previous': self.get_previous_link(),
             'results': data,
