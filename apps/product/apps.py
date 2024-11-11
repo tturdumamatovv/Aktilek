f58283aed954c61512821aecf932a11dfc7f0a48
Nurmuhammed <muki2022@mail.ru>
2024-11-11 16:42:27 +0600

    a

--- apps/product/apps.py
+++ apps/product/apps.py
@@ -5,9 +5,6 @@ class ProductConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.product'
 
-    def ready(self):
-        import apps.product.signals
-
     class Meta:
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
--- apps/product/models.py
+++ apps/product/models.py
@@ -9,6 +9,8 @@ from django.core.files.base import ContentFile
 from django.db import models
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
 
 from unidecode import unidecode
 
